a55a7fbbdfcc0c7f456b134c6ffbf5b20d37e6ff
Jongkeun <dldkfod@naver.com>
2020-01-24 23:35:45 +0900

    Updates

---
+++ precache-manifest.0f79ca95f95fd7e6a19c260b8ae3013a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8236e79cb62fc8d539d5220446d43744",
+    "url": "/Jongkeun/index.html"
+  },
+  {
+    "revision": "3bf8d6755b40e7316ff2",
+    "url": "/Jongkeun/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "fa002a3d00e627d04632",
+    "url": "/Jongkeun/static/js/2.abdae179.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Jongkeun/static/js/2.abdae179.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3bf8d6755b40e7316ff2",
+    "url": "/Jongkeun/static/js/main.ec9b8473.chunk.js"
+  },
+  {
+    "revision": "66df77d1d8ba45189aab",
+    "url": "/Jongkeun/static/js/runtime-main.efa39082.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Jongkeun/static/media/logo.5d5d9eef.svg"
+  }
+]);
