bc57bdab16030fbf83bda6ede2cdab65786e28aa
Jongkeun <dldkfod@naver.com>
2020-02-02 22:49:05 +0900

    add ArrowButton

---
+++ src/components/ArrowButton.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+const ArrowDiv = styled.div`
+  width: 20px;
+  height: 20px;
+  font-size: 3rem;
+  color: white;
+  background-color: transparent;
+  transform: translate(-50%, -50%) rotate(-90deg);
+  bottom: 0;
+`;
+const ArrowButton = () => {
+  return <ArrowDiv>{"<"}</ArrowDiv>;
+};
+
+export default ArrowButton;
--- src/pages/WelcomePage.js
+++ src/pages/WelcomePage.js
@@ -1,8 +1,17 @@
 import React from "react";
+import styled from "styled-components";
 import TypingContainer from "../containers/TypingContainer";
+import ArrowButton from "../components/ArrowButton";
+
+const Section = styled.section``;
 
 const WelcomePage = () => {
-  return <TypingContainer />;
+  return (
+    <Section>
+      <TypingContainer />
+      <ArrowButton />
+    </Section>
+  );
 };
 
 export default WelcomePage;
