2ea155c212051d697c456c894763ba99731fdfa8
Jongkeun <jongkeun.kr@gmail.com>
2020-02-06 18:02:27 +0900

    changed fragment to Switch

--- src/routers/Router.js
+++ src/routers/Router.js
@@ -1,18 +1,18 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import WelcomePage from "../pages/WelcomePage";
 import AboutPage from "../pages/AboutPage";
 
 const Router = () => {
   return (
-    <React.Fragment>
-      <Route path="/" exact>
-        <WelcomePage />
-      </Route>
+    <Switch>
       <Route path="/about">
         <AboutPage />
       </Route>
-    </React.Fragment>
+      <Route path="/">
+        <WelcomePage />
+      </Route>
+    </Switch>
   );
 };
 
