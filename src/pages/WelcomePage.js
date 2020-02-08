3bf338d102247b00d535fa99206e5ad8ee1443bf
Jongkeun <dldkfod@naver.com>
2020-02-09 00:01:17 +0900

    changed main heading text and set to center

--- src/pages/WelcomePage.js
+++ src/pages/WelcomePage.js
@@ -10,10 +10,34 @@ const Bottom = styled.div`
   left: 50%;
   margin-left: -10px;
 `;
+
+const HeadingContainer = styled.div`
+  text-align: center;
+`;
+
+const Heading = styled.h1`
+  -webkit-font-smoothing: antialiased;
+  color: #fff;
+  display: inline-grid;
+  font-family: "Playfair Display";
+  font-size: 7em;
+  font-weight: 400;
+  letter-spacing: 0;
+  line-height: 110px;
+`;
+
 const WelcomePage = () => {
   const history = useHistory();
   return (
     <LayoutContainer welcome>
+      <HeadingContainer>
+        <Heading>
+          <span>The more Humble </span>
+          <span>You are, </span>
+          <span>The Faster </span>
+          <span>You will Improve</span>
+        </Heading>
+      </HeadingContainer>
       <Bottom>
         <ArrowButton onClick={() => history.push("/about")} />
       </Bottom>
